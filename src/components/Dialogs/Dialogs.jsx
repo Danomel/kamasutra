21267df78c431ba0ccddd3e68d36a5db8158e411
Danomel <basarov06@bk.ru>
2024-03-28 20:45:45 +0500

    ДЗ 31. onClick, ref, VirtualDOM

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import React from "react";
 
 const Dialogs = (props) => {
   let dialogsElements = props.state.dialogs.map((d) => (
@@ -10,11 +11,19 @@ const Dialogs = (props) => {
   let messageElements = props.state.messages.map((m) => (
     <Message message={m.message} />
   ));
-
+  let newMessage = React.createRef();
+  let sendMessage = () => {
+    let text = newMessage.current.value;
+    alert(text);
+  };
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>{dialogsElements}</div>
-      <div className={s.messages}>{messageElements}</div>
+      <div className={s.messages}>
+        {messageElements}
+        <textarea ref={newMessage}></textarea>
+        <button onClick={sendMessage}>Отправить</button>
+      </div>
     </div>
   );
 };
