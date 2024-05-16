ed896fae3a4c9cb4c7fed4318d211771a43af588
Danomel <basarov06@bk.ru>
2024-05-16 16:31:23 +0500

    61. cookie, login в теории, auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
+import { NavLink } from "react-router-dom";
 import s from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <img
         src="https://png.pngtree.com/png-clipart/20190611/original/pngtree-wolf-logo-png-image_2306634.jpg"
         alt=""
       />
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>login</NavLink>}
+      </div>
     </header>
   );
 };
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import UsersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let rootReducer = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
   usersPage: UsersReducer,
+  auth: authReducer,
 });
 let store = configureStore({ reducer: rootReducer });
 window.store = store;
