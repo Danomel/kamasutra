9e3f6166d2341e755cb4d1d96d0c4ec5a2e82169
Danomel <basarov06@bk.ru>
2024-05-17 14:00:42 +0500

    63. DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+  headers: { "API-KEY": "0e152062-aa2b-4b2f-a212-65775f9b0b5b" },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => response.data);
+  },
+};
