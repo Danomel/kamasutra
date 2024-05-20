7c7d440577a20f95d6aa9dd71610cac1309f30f1
Danomel <basarov06@bk.ru>
2024-05-20 14:09:34 +0500

    74. componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -21,6 +21,12 @@ class ProfileStatus extends React.Component {
       status: e.currentTarget.value,
     });
   };
+  componentDidUpdate(prevProps, prevState) {
+    prevProps.status !== this.props.status &&
+      this.setState({
+        status: this.props.status,
+      });
+  }
   render() {
     return (
       <div>
